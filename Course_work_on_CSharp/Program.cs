310bd5ce6ec11cee19509c15d7486a35dffdf2fe
gamashbogdan <gamashbogdan@gmail.com>
2023-11-02 18:48:06 +0200

    Add project files.

---
+++ Course_work_on_CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Course_work_on_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
